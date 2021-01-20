5c3630c7da6eb17dfd4714f2414205ec25b05915
Hasib Islam <babulboxi99@gmail.com>
2021-01-20 15:37:24 +0600

    Initial commit

---
+++ login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log In form</title>
+</head>
+<body>
+    <p>!!Give Your All Info!!</p>
+    <?php 
+        echo  "Peace!!";
+    ?>
+</body>
+</html>
